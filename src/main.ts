554f3419da896c1b55177df6fa4c8c33ec67a95e
Abhinav shyju <abhinavshyjupc@gmail.com>
2025-09-09 17:09:40 +0530

    Fixed

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,10 @@ import { YouTubePlayer } from "./Youtube-Player";
 const videoId = "M7lc1UVf-VE";
 const containerId = "my-player-container";
 
-const player = new YouTubePlayer(containerId, videoId, {
+// const player = new YouTubePlayer(containerId, videoId, {
+new YouTubePlayer(containerId, videoId, {
   onReady: () => {
     console.log("Player is ready!");
   },
-  onStateChange: (e) => {},
+  // onStateChange: (e) => {},
 });
