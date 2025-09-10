fde247316b905e8f1be5f0573d4d075542fee17f
Abhinav shyju <abhinavshyjupc@gmail.com>
2025-09-10 20:25:55 +0530

    Website done

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,24 @@
 import { YouTubePlayer } from "./Youtube-Player";
 
-// const videoId = "wI98oxUbWSY";
-const videoId = "1qMNtY5IFac";
+const videoId = "wI98oxUbWSY";
 const containerId = "my-player-container";
 
 const player = new YouTubePlayer(containerId, videoId, {
   onReady: () => {
     console.log("Player is ready!");
   },
-  // onStateChange: (e) => {},
+  onStateChange: (e) => {
+    console.log(e.data);
+  },
 });
 
 document.getElementById("video-id-update")?.addEventListener("click", () => {
-  const newVideoId = document.getElementById("video-id") as HTMLInputElement;
-  player.changeVideo(newVideoId.value);
+  const newVideoIdEle = document.getElementById("video-id") as HTMLInputElement;
+  const newVideoId = newVideoIdEle.value;
+  if (newVideoId == "") {
+    return;
+  }
+  player.changeVideo(newVideoId);
 });
 
 document.getElementById("video-play")?.addEventListener("click", () => {
